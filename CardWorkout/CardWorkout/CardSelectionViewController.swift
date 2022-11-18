9bf9b5ffe5c62b68fb09e70804b6de3f60ce8c23
Oleh Dorozhovers <olzer123@gmail.com>
2022-11-18 16:37:49 +0100

    Added new VC  with segue from 'rules' button. Added rules into labels

--- CardWorkout/CardWorkout/CardSelectionViewController.swift
+++ CardWorkout/CardWorkout/CardSelectionViewController.swift
@@ -1,8 +1,25 @@
 import UIKit
 
 class CardSelectionViewController: UIViewController {
-
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
 
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
+    
 }
